f128a36cb17391d64bea3c57396919f0ff8a0790
Raffaele Amietta <70523797+rahffaele@users.noreply.github.com>
2023-07-20 14:21:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,125 @@
 /*
-Let's try to t the teachable machine!
+Here we check the result we get from the posnet teachable machine.
+See letters
+
+This sketch is slightly different.
+The syntax of p5 is different.
+Also need to add another library in the html file.
 */
 
-let classifier;
-let poseModelURL = "https://teachablemachine.withgoogle.com/models/WLt0TIdVI/";
+/*
+Here we load the file from teachablemachine
+*/
 
+const modelURL = "https://teachablemachine.withgoogle.com/models/WLt0TIdVI/";
+const checkpointURL = modelURL + "model.json";
+const metadataURL = modelURL + "metadata.json";
+
+const flip = true;
+
+let model;
+let totalClasses;
+let myCanvas;
+
+let classification = "None";
+let probability = "100";
+let poser;
 let video;
-let flippedVideo;
-let label = "";
-let sound, sound2;
+let sound;
+let sound2;
 
+/*
+preloadsound
+*/
 function preload() {
-  classifier = ml5.poseNet(poseModelURL);
-  sound = loadSound("../mendrisio-music.mp3");
+  sound = loadSound("mendrisio-music.mp3");
+  /*sound2 = loadSound("orchestra.wav");*/
 }
 
-function setup() {
-  createCanvas(320, 260);
-  
-  video = createCapture(VIDEO);
+/*
+Here we load the positions 
+*/
+async function load() {
+  model = await tmPose.load(checkpointURL, metadataURL);
+  totalClasses = model.getTotalClasses();
+  console.log("Number of classes, ", totalClasses);
+}
+
+async function setup() {
+  myCanvas = createCanvas(400, 400);
+  videoCanvas = createCanvas(320, 240);
+  await load();
+  video = createCapture(VIDEO, videoReady);
   video.size(320, 240);
   video.hide();
-
-  flippedVideo = ml5.flipImage(video);}
+}
 
 function draw() {
   background(0);
-  // Draw the video
-  image(flippedVideo, 0, 0);
-
- 
-  // Draw the label
-  fill(255);
+  if (video)
+    //image(video, 0, 0);
+    fill(255, 0, 0);
   textSize(16);
-  textAlign(CENTER);
-  text(label, width / 2, height - 4);
-  
-  textSize(50);
-  if(label == 'test pose') {
-      text('music stops', width / 2, height/2);
-     sound.stop();
-  } 
-  else if(label == 'Class 2') {
-    text('music plays', width / 2, height / 2);
-    if (sound.isPlaying() == false) {
+  //text("Result:" + classification, 10, 40);
+  //text("Probability:" + probability, 10, 20);
+  textSize(8);
+
+  if (probability > 0.8 && classification != "None") {
+    push();
+    textSize(200);
+    fill(255);
+    textAlign(CENTER);
+    text(classification, width / 2, height - 50);
+    pop();
+
+    // you can use thia part for to something
+    if (classification == "test pose") {
+      //play sound
+      if (sound.isPlaying() == true) {
+        sound.stop();
+      }
+    } else if (classification == "Class 2") {
       sound.play();
+    } 
+  }
+
+  if (poser) {
+    for (var i = 0; i < poser.length; i++) {
+      let x = poser[i].position.x;
+      let y = poser[i].position.y;
+      ellipse(x, y, 5, 5);
+      text(poser[i].part, x + 4, y);
     }
   }
-   else {
-    
-  }
-
 }
 
-// When we get a result
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  label = results[0].label;
-  classifyVideo();
+function videoReady() {
+  console.log("Video Ready");
+  predict();
+}
+
+/*
+This function is to be copied and pasted.
+What happens here is that we make a prediction of what the letter might be
+*/
+async function predict() {
+  const flipHorizontal = false;
+  const { pose, posenetOutput } = await model.estimatePose(
+    video.elt, //webcam.canvas,
+    flipHorizontal
+  );
+  const prediction = await model.predict(
+    posenetOutput,
+    flipHorizontal,
+    totalClasses
+  );
+
+  const sortedPrediction = prediction.sort(
+    (a, b) => -a.probability + b.probability
+  );
+
+  classification = sortedPrediction[0].className;
+  probability = sortedPrediction[0].probability.toFixed(2);
+  if (pose) poser = pose.keypoints; // is there a skeleton
+  predict();
 }
